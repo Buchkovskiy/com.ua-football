ee9d6637d341b7b6a93a07cc8e537275c34d1288
sergiy_buchkivskyi <Kraftwerke25>
2019-11-07 17:44:22 +0200

    sing in test

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class HomePage {
+
+
+    public void isEnabled() {
+        try{
+
+        }catch (Exception e){
+
+        }
+
+    }
+}
--- src/test/java/loginTest/LoginTest.java
+++ src/test/java/loginTest/LoginTest.java
@@ -6,9 +6,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
+
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.HasInputDevices;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -18,37 +21,48 @@ public class LoginTest {
     @Test
     public void loginTest() {
         //set ChromeDriver and path to file
-        System.setProperty("webdriver.chrome.driver", "F://Automation/uafootball/src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "F://Automation/uafootball/src/drivers/chromedriver_0711.exe");
         WebDriver webDriver = new ChromeDriver();
-        Actions action = new Actions(webDriver);
-
 
         //for opening window of browser in maximum size
         webDriver.manage().window().maximize();
         //for waiting before each action
-        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
         webDriver.get("https://ua-football.com");
 
+        //click on signIn button
         WebElement singIn = webDriver.findElement(By.id("authBtn"));
         singIn.click();
 
+        //input login to "Login" field
         WebElement loginField = webDriver.findElement(By.id("login"));
         loginField.clear();
         loginField.sendKeys("Fanat2019");
 
+        //input password to "Password" field
         WebElement passField = webDriver.findElement(By.id("password"));
         passField.clear();
         passField.sendKeys("Fanat2019");
 
-        WebElement politics = webDriver.findElement(By.xpath("//span[@class='btn btn-primary']"));
-        politics.click();
+        WebElement submitButton = webDriver.findElement(By.xpath(".//div[@class='d-flex justify-content-around align-items-center'] /button[@type='submit']"));
+        submitButton.click();
 
-        WebElement submitSingIn = webDriver.findElement(By.xpath(".//div[@class=\'d-flex justify-content-around align-items-center\'] //button[@type=\'submit\']"));
-        submitSingIn.click();
+       //move to singIn
+        Actions actions = new Actions(webDriver);
+        actions.moveToElement(singIn).build().perform();
 
-        action.moveToElement(singIn);
-        Assert.assertTrue("Avatar is not present", webDriver.findElement(By.xpath(".//div[text()='Fanat2019']")).isDisplayed());
+/*      WebElement singInDropbox = webDriver.findElement(By.xpath(".//li[@class='d-flex align-items-center p-3']"));
+        FluentWait<WebDriver> wait = new WebDriverWait(webDriver, 10);
+        wait.until(ExpectedConditions.visibilityOf(singInDropbox));*/
+
+        //check that we loginned on site
+        Assert.assertTrue("Fanat2019 is not enabled", webDriver.findElement(By.xpath(".//div[text()='Fanat2019']")).isEnabled());
+
+        //close browser, if i used method .close - we close only tab, no browser!
+        webDriver.close();
+
+
 
     }
 
